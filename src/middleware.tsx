14707844e3f4423aafb8f65fe1c542d4d45bc6f7
H5SH <tipuasad2001@gmail.com>
2025-09-04 09:09:05 +0100

    Middleware remove all

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,30 +1,30 @@
-// middleware.js (or middleware.ts)
-import { NextRequest, NextResponse } from 'next/server';
+// // middleware.js (or middleware.ts)
+// import { NextRequest, NextResponse } from 'next/server';
 
-// Paths that require authentication
-const protectedPaths = ['/']; // add more like '/dashboard', '/profile', etc.
+// // Paths that require authentication
+// const protectedPaths = ['/']; // add more like '/dashboard', '/profile', etc.
 
-export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
+// export async function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
 
-  // Check if path requires authentication
-  if (protectedPaths.includes(path)) {
-    // Get cookies (can't use await cookies() here, must use request.cookies)
-    const token = request.cookies.get('bigcommerce_access_token');
+//   // Check if path requires authentication
+//   if (protectedPaths.includes(path)) {
+//     // Get cookies (can't use await cookies() here, must use request.cookies)
+//     const token = request.cookies.get('bigcommerce_access_token');
 
-    // if (!token) {
-    //     console.log(token, 'token')
-    //   const url = request.nextUrl.clone();
-    //   url.pathname = '/auth';
-    //   return NextResponse.redirect(url);
-    // }
-  }
+//     // if (!token) {
+//     //     console.log(token, 'token')
+//     //   const url = request.nextUrl.clone();
+//     //   url.pathname = '/auth';
+//     //   return NextResponse.redirect(url);
+//     // }
+//   }
 
-  // Continue to the requested page
-  return NextResponse.next();
-}
+//   // Continue to the requested page
+//   return NextResponse.next();
+// }
 
-// Configure matcher for which routes should trigger the middleware
-export const config = {
-  matcher: ['/', '/dashboard', '/profile'], // protected routes
-};
+// // Configure matcher for which routes should trigger the middleware
+// export const config = {
+//   matcher: ['/'], // protected routes
+// };
